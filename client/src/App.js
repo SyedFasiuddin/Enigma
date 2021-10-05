8e413cad6f85968261df28ec6a2ee4e0567c7181
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2021-10-05 19:09:16 +0530

    Added simple todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+
+// TODO
+/* 1: Make the UI same as that in the figma design
+ * 2: take all the values from all the fields when "encrypt" button is clicked
+ * 3: Use fetch api to send a post request to nodejs server 
+ * 4: The data that is sent is in json form -> create a constructor function for this
+ * 5: Then the server returns the promise 
+ * 6: Stingify that and then display the message in the text field/div
+*/
+
 function App() {
   return (
     <div className="App">
