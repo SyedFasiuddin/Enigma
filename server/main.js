a1b5f0b769daff534bf0827abcebcb050dfdaf1d
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2021-10-05 19:08:30 +0530

    Added simple todos

---
+++ server/main.js
@@ -0,0 +1,7 @@
+/* TODO
+ * 1: Create the server and listen
+ * 2: When post method is called take the passed json object
+ * 3: consider all the rotat and plug board settings and pass the message into functions
+ * 4: Take the output of the functions and send it back to the client and complete the promise
+ * 5: when the app is deployed we also have to serve the html css and js
+*/
